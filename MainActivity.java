a1bd40e9c739160ccbdc4c41e68b61bbc0ed0e3c
Chamil <54888362+chamilwickramasinghe@users.noreply.github.com>
2020-03-26 09:06:30 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.labsheet3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        public void sendMessage(View view) {
+
+            Intent intent = new Intent(this,DisplayMessageActivity.class);
+            EditText editText = (EditText) findViewById(R.id.editText);
+            String message = editText.getText().toString();
+
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
+    }
+}
